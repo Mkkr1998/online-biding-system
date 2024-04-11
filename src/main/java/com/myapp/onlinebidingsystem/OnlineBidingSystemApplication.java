0d54f5790e19f254038e06dc6ad9ed2a14ac1a29
Manish Kumar <110426424+Mkkr1998@users.noreply.github.com>
2024-04-11 23:17:40 +0530

    project-setup

---
+++ src/main/java/com/myapp/onlinebidingsystem/OnlineBidingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.onlinebidingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBidingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBidingSystemApplication.class, args);
+	}
+
+}
